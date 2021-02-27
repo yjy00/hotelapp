5511aeabd42f050334c42ea09b3a108fef32c788
yjy00 <yushno14@gmail.com>
2021-02-28 08:50:58 +0900

    Update halls_controller.rb

---
+++ app/controllers/halls_controller.rb
@@ -0,0 +1,25 @@
+class HallsController < ApplicationController
+
+
+  def index
+  end
+
+  def new
+    @hall = Hall.new
+  end
+
+  def create
+    @hall = Hall.new(hall_params)
+    if @hall.save
+      redirect_to halls_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def hall_params
+    params.require(:hall).permit(:image, :name, :detail, :count)
+  end
+end
