74c1c15ee3418e57bcfc91abc26e987fb3df8a82
yjy00 <yushno14@gmail.com>
2021-02-28 08:27:05 +0900

    新規登録できるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'mainmenus#index'
 
   resources :mainmenus, only: :index
-  resources :halls, only: [:index, :new]
+  resources :halls, only: [:index, :new, :create]
 
 
 end
