692d12409dbc7105049421de56ca2a551e5f2687
yjy00 <yushno14@gmail.com>
2021-02-28 08:31:26 +0900

    新規登録できるようにする

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -5,7 +5,21 @@ class HallsController < ApplicationController
   end
 
   def new
-    
+    @hall = Hall.new
   end
 
+  def create
+    @hall = Hall.new(hall_params)
+    if @hall.save
+      redirect_to halls_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def hall_params
+    params.require(:hall).permit(:image, :name, :detail, :count)
+  end
 end
