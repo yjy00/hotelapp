3c0658aefc406ecad1f7397131c6d7c0c43552be
yjy00 <yushno14@gmail.com>
2021-02-28 18:09:25 +0900

    kitchenモデルの生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_082101) do
+ActiveRecord::Schema.define(version: 2021_02_28_090703) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2021_02_28_082101) do
   end
 
   create_table "kitchens", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
