523d7eece9e52e7d9ea780337ddb79b167f2de5b
yjy00 <yushno14@gmail.com>
2021-02-28 17:26:27 +0900

    アクションの生成

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,2 +1,24 @@
 class KitchensController < ApplicationController
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
 end
