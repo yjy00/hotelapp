e9b65f2b3c3053ddfa28a9d82410920fa3b09688
yjy00 <yushno14@gmail.com>
2021-02-28 21:04:34 +0900

    追加機の実装

---
+++ app/controllers/kitchen_managements_controller.rb
@@ -0,0 +1,45 @@
+class KitchenManagementsController < ApplicationController
+
+  def index
+    @kitchens = Kitchen.order('updated_at DESC')
+  end
+
+  def new
+    @kitchen = Kitchen.new
+  end
+
+  def create
+    @kitchen = Kitchen.new(kitchen_params)
+    # binding.pry
+    if @kitchen.save
+      redirect_to kitchen_managements_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @kitchen = Kitchen.find(params[:id])
+  end
+
+  def show
+    @kitchen = Kitchen.find(params[:id])
+    
+  end
+
+  def update
+    @kitchen = Kitchen.find(params[:id])
+    if @kitchen.update(kitchen_params)
+      redirect_to kitchen_managements_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def kitchen_params
+    params.require(:kitchen).permit(:name, :category_id, :count_id, :place_id, :detail)
+  end
+
+end
