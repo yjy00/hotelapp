c2fd07bb94a38804a8183d5172def41ca0614751
yjy00 <yushno14@gmail.com>
2021-03-01 17:26:40 +0900

    rubocpo

---
+++ spec/factories/kitchens.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :kitchen do
+    name              { 'mixed nuts' }
+    category_id       { 10 }
+    place_id          { 2 }
+    count_id          { 5 }
+    detail            { 'crispy mixed nuts' }
+  end
+end
