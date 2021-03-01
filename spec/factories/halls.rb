36c1c663fbb80a59001b347ff599eafaacdf1bc7
yjy00 <yushno14@gmail.com>
2021-03-01 13:17:49 +0900

    factoryのインスタンスの中身を実装

--- spec/factories/halls.rb
+++ spec/factories/halls.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    name                  {"beer glass"}
+    category_id           {9}
+    place_id              {2}
+    count_id              {10}
+    detail                {"10 boxes of new beer glasses"}
   end
 
 end
--- spec/factories/kitchens.rb
+++ spec/factories/kitchens.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    name              {"mixed nuts"}
+    category_id       {10}
+    place_id          {2}
+    count_id          {5}
+    detail            {"crispy mixed nuts"}
   end
 
 end
