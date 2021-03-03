5ce7f9e7f26ed4ae063d6f949682cb4120ec7e5c
yjy00 <yushno14@gmail.com>
2021-03-04 07:15:47 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   root to: 'mainmenus#index'
 
   resources :mainmenus,         only: :index
-  resources :halls,             only: :index
-  resources :hall_managements
-  resources :kitchens, only: :index
-  resources :kitchen_managements
   resources :foods
   resources :beverages
   resources :equipments
