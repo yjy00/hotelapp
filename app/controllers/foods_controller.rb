cf5a95ebca3280214ba0b99a67ed7519ad9b85c6
yjy00 <yushno14@gmail.com>
2021-03-03 16:22:27 +0900

    foodコントローラーの編集

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,32 +1,42 @@
 class FoodsController < ApplicationController
 
   def index
-    
+    @goods = Food.order('updated_at DESC')
   end
 
   def new
-    
+    @food = Food.new
   end
 
   def create
-    
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
-    
+    @food = Food.find(params[:id])
   end
 
   def show
-    
+    @food = Food.find(params[:id])
   end
 
   def update
-    
+    @food = Food.find(params[:id])
+    if @food.update(food_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def food_params
-    
+    params.require(:food).permit()
   end
 end
