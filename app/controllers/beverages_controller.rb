a1c534ab7992c93bdd4bc82c7b6197b986cae60d
yjy00 <yushno14@gmail.com>
2021-03-03 16:15:49 +0900

    beverageコントローラーの初期設定

---
+++ app/controllers/beverages_controller.rb
@@ -0,0 +1,32 @@
+class BeveragesController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def beverage_params
+    
+  end
+end
