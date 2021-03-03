07c2d8b99f1d2361bc1473d66358413e69cb12da
yjy00 <yushno14@gmail.com>
2021-03-03 16:14:18 +0900

    foodコントローラーの初期設定

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,32 @@
+class FoodsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def food_params
+    
+  end
+end
