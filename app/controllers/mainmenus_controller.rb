78e32d3b58e040fd17b813ef793bf053da5afe33
yjy00 <yushno14@gmail.com>
2021-03-04 07:34:38 +0900

    メインメニューindexアクションの修正

--- app/controllers/mainmenus_controller.rb
+++ app/controllers/mainmenus_controller.rb
@@ -1,13 +1,6 @@
 class MainmenusController < ApplicationController
 
-  # def index
-  #   posts = Post.all
-  #   ads = Advertisement.all
-  
-  #   # それぞれの複数インスタンスを1つの配列にする
-  #   @instances = posts | ads
-  #   # 作成降順に並び替え
-  #   @instances.sort!{ |a, b| b.created_at <=> a.created_at }
-  # end
+  def index
+  end
 
 end
