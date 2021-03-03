3197a19793a9c2c90860550b448b5c8c5b36b38b
yjy00 <yushno14@gmail.com>
2021-03-03 16:16:57 +0900

    equipmentsコントローラーの初期設定

---
+++ app/controllers/equipments_controller.rb
@@ -0,0 +1,32 @@
+class EquipmentsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def equipment_params
+    
+  end
+end
