72f58d7f0659f909863fe8bbfab9e7444f31750f
yjy00 <yushno14@gmail.com>
2021-03-10 14:13:12 +0900

    アクションの定義

--- app/controllers/daily_handovers_controller.rb
+++ app/controllers/daily_handovers_controller.rb
@@ -1,2 +1,22 @@
 class DailyHandoversController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
 end
