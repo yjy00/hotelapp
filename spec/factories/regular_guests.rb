2b30721c110ad9189c5df872b7b83b5b14aa99f3
yjy00 <yushno14@gmail.com>
2021-03-10 18:12:03 +0900

    factoriesの実装

--- spec/factories/regular_guests.rb
+++ spec/factories/regular_guests.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :regular_guest do
-    
+    name                  { '吉田敦' }
+    status_id              { 2 }
+    member_number              { 33326482746 }
+    information                { 'Titaniumのゲストです。名前を覚え、しっかり挨拶できるようにしましょう。' }
   end
 end
