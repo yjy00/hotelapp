bd525e4ac6b62b5c45cf57ec4f6db618c49ad40c
yjy00 <yushno14@gmail.com>
2021-03-10 19:12:31 +0900

    モデル、マイグレーションファイルの実装

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,10 @@
 class Menu < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  has_one_attached :image
+  belongs_to :category
+
+  validates :name, :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_080005) do
+ActiveRecord::Schema.define(version: 2021_03_10_100453) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -102,6 +102,14 @@ ActiveRecord::Schema.define(version: 2021_03_10_080005) do
     t.integer "place_id", null: false
     t.integer "count_id", null: false
     t.text "detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "menus", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "category_id", null: false
+    t.text "information"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
