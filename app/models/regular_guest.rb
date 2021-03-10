2ad981ac30d8188da54c2c085dcd8101f4733d4a
yjy00 <yushno14@gmail.com>
2021-03-10 17:11:09 +0900

    ActiveHashの準備

--- app/models/regular_guest.rb
+++ app/models/regular_guest.rb
@@ -1,2 +1,8 @@
 class RegularGuest < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :status
+
+  validates :name, presence: true
+  validates :status_id, numericality: { other_than: 1 }
 end
