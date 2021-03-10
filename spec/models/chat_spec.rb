72074a72183c84896c98b770ec9e5df4eebfd1e5
yjy00 <yushno14@gmail.com>
2021-03-10 13:27:57 +0900

    テストの実装

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Chat do
+    describe '#create' do
+      before do
+        @chat = build(:chat)
+      end
+
+      context '登録されること' do
+        it '全てのフォームが入力されていれば登録できること' do
+          expect(@chat).to be_valid
+        end
+      end
+
+      context '登録されないこと' do
+        it 'fromが必須であること' do
+          @chat.from_id = nil
+          @chat.valid?
+          expect(@chat.errors[:from_id]).to include("can't be blank")
+        end
+
+        it 'byが必須であること' do
+          @chat.by = nil
+          @chat.valid?
+          expect(@chat.errors[:by]).to include("can't be blank")
+        end
+
+        it 'messageが必須であること' do
+          @chat.message = nil
+          @chat.valid?
+          expect(@chat.errors[:message]).to include("can't be blank")
+        end
+      end
+      
+    end
+  end
 end
