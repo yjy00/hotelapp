28f79f42ec1d68c2c7e95204540094c7f5fe0ace
yjy00 <yushno14@gmail.com>
2021-03-10 17:06:18 +0900

    migration fileの実装

--- db/migrate/20210310080005_create_regular_guests.rb
+++ db/migrate/20210310080005_create_regular_guests.rb
@@ -1,6 +1,10 @@
 class CreateRegularGuests < ActiveRecord::Migration[6.1]
   def change
     create_table :regular_guests do |t|
+      t.string  :name,           null: false
+      t.integer :status_id
+      t.integer :member_number
+      i.text    :information
 
       t.timestamps
     end
