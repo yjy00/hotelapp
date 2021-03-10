39ebdb6027f9cf8dff532322580b9fd76e0a79d7
yjy00 <yushno14@gmail.com>
2021-03-10 15:51:31 +0900

    テストの実装

--- spec/factories/daily_handovers.rb
+++ spec/factories/daily_handovers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :daily_handover do
-    
+    month                    { 1 }
+    date                     { 1 }
+    daily_remark             { 'ランチとディナーNew Years Buffet開催。' }
+    to_morning               { 'お子様連れが多いと思いますので、子供椅子の確保をお願いします。' }
+    to_evening               { '忙しくなると思うので、ライム等多めに準備を忘れないようにしてください。' }
+    for_tomorrow             { '明日の魚は鮭だそうです。' }
+  
   end
 end
