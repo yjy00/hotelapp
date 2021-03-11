88d9f1e1e014b68a6fb2f3b99a68b18f54bfcbb8
yjy00 <yushno14@gmail.com>
2021-03-11 09:40:23 +0900

    factoriesの修正

--- spec/factories/beverages.rb
+++ spec/factories/beverages.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :beverage do
     name                  { '知多ウイスキー' }
     place_id              { 2 }
-    count_id              { 3 }
+    count                 { 3 }
     detail                { 'ハイボールのために作られたウイスキー' }
   end
 end
--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :equipment do
     name                  { 'ビーツグラる' }
     place_id              { 2 }
-    count_id              { 10 }
+    count                 { 10 }
     detail                { 'ビールグラス　10箱　6個入り' }
   end
 end
