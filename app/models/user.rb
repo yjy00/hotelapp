23f393b661f11a3d10bf87d90d8667aded003426
yjy00 <yushno14@gmail.com>
2021-03-12 14:12:05 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, :managers_name, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
-
 end
