8e57b0a65a35e3543d503069e8126ef3f19f6a03
yjy00 <yushno14@gmail.com>
2021-03-12 12:59:15 +0900

    ログインのための実装(application_controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
+  before_action :configure_permitted_parameters
+
+  protected
+ 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :managers_name])
+  end
+ 
+ 
+
 end
