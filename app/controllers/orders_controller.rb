78aa98addfd55d7ad752e60bb80388d5c23d7dfe
yjy00 <yushno14@gmail.com>
2021-03-12 09:21:29 +0900

    ホール側の発注リスト実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+
   def index
     beverages = Beverage.where('count <= 2').order('updated_at DESC')
     equipments = Equipment.where('count <= 2').order('updated_at DESC')
@@ -8,4 +9,5 @@ class OrdersController < ApplicationController
     # 作成降順に並び替え
     @instances.sort! { |a, b| b.updated_at <=> a.updated_at }
   end
+
 end
