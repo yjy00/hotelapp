fb4b39f57cae8f7fe912d6c0ae9b90f2f91a33f7
yjy00 <yushno14@gmail.com>
2021-03-14 12:47:06 +0900

    編集の際に現在のパスワードが入らないようにする

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,7 +38,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :managers_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
