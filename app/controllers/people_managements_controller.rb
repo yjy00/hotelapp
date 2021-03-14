ae8e0b20e17f3cb4006ec0ac64898babf005dd75
yjy00 <yushno14@gmail.com>
2021-03-15 08:40:12 +0900

    コントローラーの実装

--- app/controllers/people_managements_controller.rb
+++ app/controllers/people_managements_controller.rb
@@ -1,2 +1,21 @@
 class PeopleManagementsController < ApplicationController
+
+  def edit
+    @people_management = PeopleManagement.find(1)
+  end
+
+  def update
+    @people_management = PeopleManagement.find(1)
+    if @people_management.update(people_management_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def people_management_params
+    params.require(:people_management).permit(:morning, :waiter, :kitchen, :dish_wash)
+  end
 end
