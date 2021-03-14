76536d7986234e6d33ac933f620882af160ae196
yjy00 <yushno14@gmail.com>
2021-03-15 07:34:50 +0900

    マイグレーションファイルの実装

--- db/migrate/20210314223148_create_people_managements.rb
+++ db/migrate/20210314223148_create_people_managements.rb
@@ -1,6 +1,10 @@
 class CreatePeopleManagements < ActiveRecord::Migration[6.1]
   def change
     create_table :people_managements do |t|
+      t.integer :morning
+      t.integer :waiter
+      t.integer :kitchen
+      t.integer :dish_wash
 
       t.timestamps
     end
