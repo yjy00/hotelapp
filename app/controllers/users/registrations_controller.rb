5292c764b98c7bfea09f7dc633236eda8d2a96e1
yjy00 <yushno14@gmail.com>
2021-03-14 16:17:30 +0900

    削除できないようにする

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :check_guest, only: :destroy
 
+  def check_guest
+    if resource.email == 'sheroton.fb@sheroton.com'
+      redirect_to root_path
+    end
+  end
   # GET /resource/sign_up
   # def new
   #   super
