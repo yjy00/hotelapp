cc6545955cdc4c51584266a50c32346e2e1de84f
yjy00 <yushno14@gmail.com>
2021-03-24 17:16:20 +0900

    capistranoを元に戻す

--- hotelapp/config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-server '3.115.181.127', user: 'hotelapp', roles: %w{app db web}
-
-set :stage, :production
-set :branch, "master"
-set :rails_env, "production"
-
-role :app, %w{USER_NAME@IP_ADDRESS}
