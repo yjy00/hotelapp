4f3517ffc60813e250ef4d5ed170ecb5a29a3652
yjy00 <yushno14@gmail.com>
2021-03-25 14:54:50 +0900

    Capistrano導入用の実装

--- hotelapp/config/deploy.rb
+++ hotelapp/config/deploy.rb
@@ -10,11 +10,18 @@ set :repo_url,  'git@github.com:yjy00/hotelapp.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
 set :rbenv_type, :user
 set :rbenv_ruby, '3.0.0'
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/hotelapp05.pem'] 
+set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/hotelapp05_key_rsa'] 
 
 set :keep_releases, 5
+
+# Default branch is :master
+set :branch, ENV['BRANCH'] || 'master'
+
+set :user, 'hotelapp'
+
+SSHKit.config.command_map.prefix[:puma].push("bundle exec")
+SSHKit.config.command_map.prefix[:pumactl].push("bundle exec")
