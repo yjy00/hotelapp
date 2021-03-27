d2672a2097f13f7271e2a2c639ee51c08063f047
yjy00 <yushno14@gmail.com>
2021-03-28 08:43:58 +0900

    コントローラーの実装

--- hotelapp/app/controllers/items_controller.rb
+++ hotelapp/app/controllers/items_controller.rb
@@ -1,2 +1,53 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.order('updated_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :count, :place_id, :category_id, :detail)
+  end
+end
+
 end
