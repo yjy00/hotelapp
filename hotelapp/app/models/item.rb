affb47c18d6c704ca4e2b2cbca2f070a9401fbf7
yjy00 <yushno14@gmail.com>
2021-03-28 08:45:17 +0900

    modelの実装

--- hotelapp/app/models/item.rb
+++ hotelapp/app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  has_one_attached :image
+  belongs_to :place
+  belongs_to :category
+
+  validates :name, :count, :place_id, :category_id,presence: true
+  validates :place_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
 end
