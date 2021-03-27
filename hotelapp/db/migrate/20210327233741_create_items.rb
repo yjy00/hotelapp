41ef3f3e58cc2e781464e601a6c69e34ad0886fe
yjy00 <yushno14@gmail.com>
2021-03-28 08:40:54 +0900

    DBの中身を作成

--- hotelapp/db/migrate/20210327233741_create_items.rb
+++ hotelapp/db/migrate/20210327233741_create_items.rb
@@ -1,6 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
+      t.string :name, null: false
+      t.integer :category_id, null: false
+      t.integer :place_id, null: false
+      t.integer :count, null:false
+      t.text :detail
 
       t.timestamps
     end
--- hotelapp/db/schema.rb
+++ hotelapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_223148) do
+ActiveRecord::Schema.define(version: 2021_03_27_233741) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -91,6 +91,16 @@ ActiveRecord::Schema.define(version: 2021_03_14_223148) do
     t.integer "category_id", null: false
     t.integer "place_id", null: false
     t.integer "count_id", null: false
+    t.text "detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "category_id", null: false
+    t.integer "place_id", null: false
+    t.integer "count", null: false
     t.text "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
