3888ea663f25e2f907245028291d1cb54a9a7f00
yjy00 <yushno14@gmail.com>
2021-03-28 15:07:08 +0900

    テストの実装

--- hotelapp/spec/factories/items.rb
+++ hotelapp/spec/factories/items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { '知多和牛のステーキ' }
+    category_id           { 3 }
+    place_id              { 3 }
+    count                 { 3 }
+    detail                { '焼き加減を選べます。' }
   end
 end
