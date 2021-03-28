12479122412d0ec19ab40628060068dd1084f384
yjy00 <yushno14@gmail.com>
2021-03-29 08:13:23 +0900

    ユーザー編集のリダイレクト先を修正

--- hotelapp/app/controllers/users/registrations_controller.rb
+++ hotelapp/app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: [:destroy, :update]
 
   def check_guest
-    redirect_to root_path if resource.email == 'sheroton.fb@sheroton.com'
+    redirect_to other_functions_path if resource.email == 'sheroton.fb@sheroton.com'
   end
   # GET /resource/sign_up
   # def new
