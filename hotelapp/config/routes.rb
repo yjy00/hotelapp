3da0b9c577b242dc976302df1e1973553ee5b8f0
yjy00 <yushno14@gmail.com>
2021-03-28 09:20:56 +0900

    rootの変更

--- hotelapp/config/routes.rb
+++ hotelapp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'foods#index'
+  root to: 'items#index'
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
