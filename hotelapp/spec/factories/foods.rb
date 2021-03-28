d7ac194632ad7e670b9ffd80b36a371da373deab
yjy00 <yushno14@gmail.com>
2021-03-28 15:07:30 +0900

    不要なテストファイルの削除

--- hotelapp/spec/factories/foods.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :food do
-    name                  { '知多和牛のステーキ' }
-    place_id              { 3 }
-    count                 { 3 }
-    detail                { '焼き加減を選べます。' }
-  end
-end
