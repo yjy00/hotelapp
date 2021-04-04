2bdaf2f6056890327c417bdb37a7b632ce07be33
yjy00 <yushno14@gmail.com>
2021-04-04 17:00:21 +0900

    capistrano実装

--- hotelapp/config/puma.rb
+++ hotelapp/config/puma.rb
@@ -16,11 +16,11 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 
-app_path = File.expand_path('', __FILE__)
+# app_path = File.expand_path('', __FILE__)
 
 # port ENV.fetch("PORT", 3000)
-bind "unix://#{app_path}/shared/tmp/sockets/puma.sock"
-# bind "unix://#{Rails.root}/shared/tmp/sockets/puma.sock"
+# bind "unix://#{app_path}/shared/tmp/sockets/puma.sock"
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
