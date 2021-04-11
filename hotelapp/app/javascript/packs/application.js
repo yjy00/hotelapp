57a297f5a97723415e9077784878ac2dfc5ccafa
yjy00 <yushno14@gmail.com>
2021-04-11 17:14:08 +0900

    jqueryの読み込み

--- hotelapp/app/javascript/packs/application.js
+++ hotelapp/app/javascript/packs/application.js
@@ -3,7 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+
 import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
