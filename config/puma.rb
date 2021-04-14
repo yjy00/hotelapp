517d1701c4df32b1520996eb02910c6d1ebcde2f
yjy00 <yushno14@gmail.com>
2021-04-14 17:24:10 +0900

    capistrano実装

--- config/puma.rb
+++ config/puma.rb
@@ -18,7 +18,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 
 # port ENV.fetch("PORT", 3000)
-bind "unix://#{Rails.root}/tmp/shared/sockets/puma.sock"
+bind "unix://#{Rails.root}/shared/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
