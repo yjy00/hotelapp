70aadd097a026c027cd80eba7ed1f3fc9d963b49
yjy00 <yushno14@gmail.com>
2021-04-17 17:04:12 +0900

    capistrano実装

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,8 @@ lock "~> 3.14.1"
 set :application, "hotelapp"
 set :repo_url, "git@github.com:yjy00/hotelapp.git"
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'config/master.key')
-
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 set :rbenv_type, :user
 
 set :rbenv_ruby, '3.0.0'
