375cfe9cd458ea9783235b81274dd732b8950717
yjy00 <yushno14@gmail.com>
2021-04-23 14:46:55 +0900

    bootstrap実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@
 //= require turbolinks
 //= require_tree .
 
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
