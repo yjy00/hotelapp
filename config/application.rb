72cafab02aaa62fb2ca24138206e4942dcd7d96e
yjy00 <yushno14@gmail.com>
2021-04-29 14:11:21 +0900

    タイムゾーンを日本に変更

--- config/application.rb
+++ config/application.rb
@@ -25,3 +25,12 @@ module Hotelapp
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
+
+module TimeFormatSandbox
+  class Application < Rails::Application
+
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Asia/Tokyo'
+  end
+end
