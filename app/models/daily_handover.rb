abf885a93f0d02a24b9bc615379097955d71d335
yjy00 <yushno14@gmail.com>
2021-05-04 14:44:49 +0900

    バリデーションの変更

--- app/models/daily_handover.rb
+++ app/models/daily_handover.rb
@@ -2,4 +2,6 @@
 
 class DailyHandover < ApplicationRecord
   validates :month, :date, presence: true
+  validates :month, numericality: { greater_than_or_equal_to: 0 }
+  validates :date, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/regular_guest.rb
+++ app/models/regular_guest.rb
@@ -6,5 +6,4 @@ class RegularGuest < ApplicationRecord
   belongs_to :status
 
   validates :name, presence: true
-  validates :member_number, allow_blank: true, numericality: { only_integer: true }, length: { in: 0..11 }
 end
