c658e1c4e72caad067b7538ede8608635ff3239a
yjy00 <yushno14@gmail.com>
2021-05-09 14:36:37 +0900

    chatコントローラーのテスト実装

---
+++ spec/controllers/chats_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ChatsController, type: :controller do
+
+  describe 'GET #index' do
+    it "@chatに正しい値が入っていること" do
+      # chats = create_list(:chat, 3) 
+      # get :index
+      # expect(assigns(:chats)).to match(items.sort{ |a, b| b.created_at <=> a.created_at } )
+  end
+    end
+
+    it "index.html.erbに遷移すること" do
+      get :index
+      expect(response).to render_template :index
+    end
+
+end
