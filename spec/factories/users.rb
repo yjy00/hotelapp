996063ee3c25532ffea098adb389c9ddf267fa5f
yjy00 <yushno14@gmail.com>
2021-05-09 14:38:59 +0900

    userのfactoryの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,9 +3,11 @@
 FactoryBot.define do
   factory :user do
     name                  { 'Sheroton Gran Tokyu Beach' }
-    email                 { 'sgtb.fb@sheroton.com' }
+    # email                 { 'sgtb.fb@sheroton.com' }
+    sequence(:email)      {Faker::Internet.email}
     password              { 'stbgmskf2fj' }
     password_confirmation { 'stbgmskf2fj' }
     managers_name         { '田口 美沙子' }
+
   end
 end
