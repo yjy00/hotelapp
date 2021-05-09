51d77f012369d80f551dd8808571265da3091ba1
yjy00 <yushno14@gmail.com>
2021-05-09 14:37:07 +0900

    foodsコントローラーのテスト実装

---
+++ spec/controllers/foods_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe FoodsController, type: :controller do
+
+  describe 'GET #index' do
+    it "@itemに正しい値が入っていること" do
+      # items = create_list(:item, 3) 
+      # get :index
+      # expect(assigns(:items)).to match(items.sort{ |a, b| b.created_at <=> a.created_at } )
+  end
+    end
+
+    it "index.html.erbに遷移すること" do
+      get :index
+      expect(response).to render_template :index
+    end
+
+end
