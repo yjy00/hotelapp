ae90ae27dda63a15920561ada93b182504ac0da2
yjy00 <yushno14@gmail.com>
2021-05-09 14:38:40 +0900

    regular guestのfactoryの修正

--- spec/factories/regular_guests.rb
+++ spec/factories/regular_guests.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :regular_guest do
     name                  { '吉田敦' }
     status_id             { 2 }
-    member_number         { 33_326_482_746 }
-    information           { 'Titaniumのゲストです。名前を覚え、しっかり挨拶できるようにしましょう。' }
+    member_number         { 333264827 }
+    information           { 'プラチナのゲストです。名前を覚え、しっかり挨拶できるようにしましょう。' }
   end
 end
