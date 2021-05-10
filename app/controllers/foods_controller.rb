73b6efecea2ee2ab7e5ffb84a237e9793f4be012
yjy00 <yushno14@gmail.com>
2021-05-10 18:49:29 +0900

    searchアクションの定義

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,4 +4,10 @@ class FoodsController < ApplicationController
   def index
     @items = Item.where(category_id: 2..7).order('updated_at DESC')
   end
+
+  def search
+    selection = params[:category]
+    @items = Item.sort(selection)
+  end
+
 end
