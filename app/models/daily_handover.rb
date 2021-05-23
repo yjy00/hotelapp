61e102f0bba892254aab59d27fc47614653f7412
yjy00 <yushno14@gmail.com>
2021-05-23 16:35:06 +0900

    文字数制限の追加

--- app/models/daily_handover.rb
+++ app/models/daily_handover.rb
@@ -4,4 +4,9 @@ class DailyHandover < ApplicationRecord
   validates :month, :date, presence: true
   validates :month, numericality: { greater_than_or_equal_to: 1 }
   validates :date, numericality: { greater_than_or_equal_to: 1 }
+
+  validates :daily_remark, length: { maximum: 1000 }
+  validates :to_morning, length: { maximum: 1000 }
+  validates :to_evening, length: { maximum: 1000 }
+  validates :for_tomorrow, length: { maximum: 1000 }
 end
