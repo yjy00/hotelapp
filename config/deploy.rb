7daa9e1c959ed4eb7df5778683ef45accf14ce02
yjy00 <yushno14@gmail.com>
2021-05-27 10:42:50 +0900

    rubocop

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,6 @@ set :deploy_to, '/var/www/rails/hotelapp'
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, '/etc/nginx/conf.d'
 
-
 # invoke 'dotenv:read'
 # invoke 'dotenv:check'
 # invoke 'dotenv:setup'
