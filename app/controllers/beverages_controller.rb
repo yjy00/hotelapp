51928fcd4e64168e1ab764e64f9704aa3315d977
yjy00 <yushno14@gmail.com>
2021-06-13 08:47:01 +0900

    サーチアクションの定義

--- app/controllers/beverages_controller.rb
+++ app/controllers/beverages_controller.rb
@@ -4,4 +4,9 @@ class BeveragesController < ApplicationController
   def index
     @items = Item.where(category_id: 8..9).order('updated_at DESC')
   end
+
+  def search
+    selection = params[:category]
+    @items = Item.sort(selection)
+  end
 end
